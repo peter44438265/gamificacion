dea6f8c4c3a3fb038b21d47fb76fc8e51ae8eb98
peter_azanero <peterazamen@gmail.com>
2021-11-27 15:45:39 -0500

    layout en modulo

--- frontend/modules/dashboard/views/layouts/dashboard-app.php
+++ frontend/modules/dashboard/views/layouts/dashboard-app.php
@@ -1,3 +1,22 @@
-<div>
-    prueba de layout
+<!DOCTYPE html>
+
+<?php $this->beginContent('@app/web/themes/gamify/views/layouts/main.php'); ?>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12 col-12">
+        <pan>panel</span>
+        <pan>panel2</span>
+        <pan>panel3</span>
+    </div>
+  </div>
+  <div class = "row">
+      <div class = "col-12">
+          <div class = "card">
+              <div class="card-body">
+                  <?= $content; ?>
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
+<?php $this->endContent(); ?>
